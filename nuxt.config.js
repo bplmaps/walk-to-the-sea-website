03c0db852ae39617da3f791b68c4531829044ef2
Alex Knoll <alex@agk.io>
2022-07-25 16:52:30 -0600

    add Google Fonts config

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ export default {
         name: 'description',
         content: 'The Walk to the Sea covers four centuries of Boston history. Beginning at the State House on Beacon Hill, overlooking the old Boston Common, the Walk passes historic monuments and skyscrapers.'
       }
+    ],
+    link: [
+      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=DM+Serif+Text&display=swap' }
     ]
   },
 
