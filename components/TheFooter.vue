ad93a1c50024def121cdb98197440d81b4cf4eea
Alex Knoll <alex@agk.io>
2022-10-27 19:21:44 -0400

    add designed by Ummo message to footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -11,20 +11,26 @@
           <nuxt-img class="h-7 w-auto sm:h-10 xl:h-18" src="/bpl-logo.png" sizes="sm:224px md:320px lg:576px xl:576px 2xl:576px" alt="Normal V. Leventhal Map & Education Center logo" />
         </a>
       </div>
-      <nav class="flex flex-wrap justify-end items-center space-x-2 xl:space-x-4">
-        <a href="mailto:info@leventhalmap.org">
-          <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-mail.svg" alt="Email icon" />
-        </a>
-        <a href="https://www.instagram.com/walktotheseaboston/" target="_blank">
-          <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-instagram.svg" alt="Instagram icon" />
-        </a>
-        <a href="https://www.facebook.com/WalkToTheSeaBos/" target="_blank">
-          <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-facebook.svg" alt="Facebook icon" />
-        </a>
-        <a href="https://twitter.com/WalkToTheSeaBos" target="_blank">
-          <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-twitter.svg" alt="Twitter icon" />
-        </a>
-      </nav>
+
+      <div class="flex justify-end items-center">
+        <p class="font-sans text-xs italic mr-4 lg:text-base lg:mr-8">
+          Designed by Ummo
+        </p>
+        <nav class="flex flex-wrap justify-end items-center space-x-2 xl:space-x-4">
+          <a href="mailto:info@leventhalmap.org">
+            <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-mail.svg" alt="Email icon" />
+          </a>
+          <a href="https://www.instagram.com/walktotheseaboston/" target="_blank">
+            <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-instagram.svg" alt="Instagram icon" />
+          </a>
+          <a href="https://www.facebook.com/WalkToTheSeaBos/" target="_blank">
+            <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-facebook.svg" alt="Facebook icon" />
+          </a>
+          <a href="https://twitter.com/WalkToTheSeaBos" target="_blank">
+            <nuxt-img class="h-6 w-auto xl:h-12" src="/icon-twitter.svg" alt="Twitter icon" />
+          </a>
+        </nav>
+      </div>
     </div>
   </footer>
 </template>
